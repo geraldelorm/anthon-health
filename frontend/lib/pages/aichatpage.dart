b9303290c42cce2fe7beba3241d618e8023a6170
Gerald E. Gbagbe <gerald.elorm.gbagbe@gmail.com>
2023-09-28 04:07:47 +0000

    AI health assistant added with dialog flow

---
+++ frontend/lib/pages/aichatpage.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:messagingapp/pages/home.dart';
+import 'package:dialog_flowtter/dialog_flowtter.dart';
+import 'package:messagingapp/pages/messages.dart';
+
+class ScheduleChatPage extends StatefulWidget {
+  const ScheduleChatPage({super.key});
+
+  @override
+  State<ScheduleChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ScheduleChatPage> {
+  late DialogFlowtter dialogFlowtter;
+  final TextEditingController _controller = TextEditingController();
+
+  List<Map<String, dynamic>> messages = [];
+
+  @override
+  void initState() {
+    DialogFlowtter.fromFile().then((instance) => dialogFlowtter = instance);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF553370),
+      body: Container(
+        padding: EdgeInsets.only(top: 60.0),
+        child: Stack(
+          children: [
+            Container(
+                margin: EdgeInsets.only(top: 50.0),
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height / 1.12,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        topRight: Radius.circular(30))),
+                child: MessagesScreen(messages: messages)),
+            Padding(
+              padding: const EdgeInsets.only(left: 10.0),
+              child: Row(
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushReplacement(context,
+                          MaterialPageRoute(builder: (context) => Home()));
+                    },
+                    child: Icon(
+                      Icons.arrow_back_ios_new_outlined,
+                      color: Color(0Xffc199cd),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 90.0,
+                  ),
+                  Text(
+                    "Anthon AI Bot",
+                      style: TextStyle(
+                        color: Color(0Xffc199cd),
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 20.0, right: 20.0, bottom: 20.0),
+              alignment: Alignment.bottomCenter,
+              child: Material(
+                elevation: 5.0,
+                borderRadius: BorderRadius.circular(30),
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30)),
+                  child: TextField(
+                    controller: _controller,
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Type a message",
+                        hintStyle: TextStyle(color: Colors.black45),
+                        suffixIcon: GestureDetector(
+                            onTap: () {
+                              sendMessage(_controller.text);
+                              _controller.clear();
+                            },
+                            child: Icon(Icons.send_rounded))),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+
+sendMessage(String text) async {
+    if (text.isEmpty) {
+      print('Message is empty');
+    } else {
+      setState(() {
+        addMessage(Message(text: DialogText(text: [text])), true);
+      });
+
+      DetectIntentResponse response = await dialogFlowtter.detectIntent(
+          queryInput: QueryInput(text: TextInput(text: text)));
+      if (response.message == null) return;
+      setState(() {
+        addMessage(response.message!);
+      });
+    }
+  }
+
+  addMessage(Message message, [bool isUserMessage = false]) {
+    messages.add({'message': message, 'isUserMessage': isUserMessage});
+  }
+
+}
+
+
